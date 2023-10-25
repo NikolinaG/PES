1da0cb4650ecacaac06442bd3486e310adeea07c
NikolinaG <64231877+NikolinaG@users.noreply.github.com>
2023-10-25 13:40:23 +0200

    Add files via upload

---
+++ prve vjezbe/main.py
@@ -0,0 +1,13 @@
+x = int(input("Unesite broj x:"))
+rezultat = 3 * x + 5
+print("Rezultat je:", rezultat)
+
+x = int(input("Unesite broj x:"))
+rezultat = 3 * x + 5
+print("Rezultat je:", rezultat)
+while x != 0:
+    x = int(input("Unesite broj x:"))
+    if(x == 0):
+        break
+
+
